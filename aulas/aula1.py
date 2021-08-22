578eb80b31e53d8659a1666eadfc0785c231fbec
filipa1992 <a.filipa.m.araujo@gmail.com>
2021-08-22 20:52:47 +0100

    primeira aula

---
+++ aulas/aula1.py
@@ -0,0 +1,11 @@
+print('Hello World!')
+
+laranjas = 5
+resposta = input("quantas laranjas tenho?")
+resposta = int(resposta)
+if resposta == laranjas:
+    print("certo")
+else:
+    print("não é o numero de laranjas que tenho")
+
+
